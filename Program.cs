a97083381624f57ce7f3134e994da1363c8070cf
Rémi Perche <rmi.percher@gmail.com>
2023-03-06 12:10:12 +0100

    Salaire et calcul du net

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Entrez votre salaire brut : ");
+        int salary = int.Parse(Console.ReadLine());
+        Console.WriteLine("Entrez la taxe : ");
+        int tax = int.Parse(Console.ReadLine());
+        Console.WriteLine("Votre salaire est de : " + salary);
+        Console.WriteLine("La taxe est de : " + tax);
+
+        Console.WriteLine("Si votre salaire est de " + salary + " et la taxe de " + tax + " alors votre salaire net est de " + AfficherSalaireNet(salary,tax));
+    }
+    static int AfficherSalaireNet(int salary, int tax)
+    {
+        return (salary * (1 - tax / 100)) / 12;
+    }    
+}
