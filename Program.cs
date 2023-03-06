86c242713edac2a28b7d0a09fa251b161d99040e
Rémi Perche <rmi.percher@gmail.com>
2023-03-06 16:00:48 +0100

    Salaire mensuel plus prime plus mois d'août

--- Program.cs
+++ Program.cs
@@ -2,38 +2,89 @@
 {
     static void Main(string[] args)
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        string[] month;
+        month = new string[13];
+        month[1] = "Janvier";
+        month[2] = "Février";
+        month[3] = "Mars";
+        month[4] = "Avril";
+        month[5] = "Mai";
+        month[6] = "Juin";
+        month[7] = "Juillet";
+        month[8] = "Août";
+        month[9] = "Septembre";
+        month[10] = "Octobre";
+        month[11] = "Novembre";
+        month[12] = "Décembre";
+        Console.WriteLine("Entrez votre salaire mensuel : ");
+        int salary = int.Parse(Console.ReadLine());
+        foreach (string mois in month)
+        {
+            double salaryMensuel = salary;
+            switch (mois)
+            {
+                case "Décembre":
+                    salaryMensuel *= 1.1;
+                    break;
+                case "Août":
+                    salaryMensuel = 0;
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine($"Salaire pour " + mois + " : " + salaryMensuel);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    static int CalculateSalaryNet(int salary, int tax)
+    {
+        return (salary * (1 - tax / 100)) / 12;
+    }
+
+    static void Salaire()
+    {
         Console.WriteLine("Entrez votre salaire brut : ");
         if (int.TryParse(Console.ReadLine(), out int salary))
         {
             Console.WriteLine("Entrez la taxe : ");
             if (int.TryParse(Console.ReadLine(), out int tax))
             {
-                int salaryNet = CalculateSalaryNet(salary,tax);
+                int salaryNet = CalculateSalaryNet(salary, tax);
                 Console.WriteLine("Si votre salaire est de " + salary + " et la taxe de " + tax + " alors votre salaire net est de " + salaryNet);
-                if (salary > 50000)
+                switch (salary)
                 {
-                    Console.WriteLine("Vous devriez faire des dons pour diminuer vos impots");
-                } else if (salaryNet < 1500)
-                {
-                    Console.WriteLine("C'est normal pour un alternant :)");
-                } else if (salary < 40000 && salary > 30000)
-                {
-                    Console.WriteLine("Tu devrais prendre une formation bac+5 en dev au CESI pour passer ingénieur");
+                    case > 50000:
+                        Console.WriteLine("Vous devriez faire des dons pour diminuer vos impots");
+                        break;
+                    case < 1500*12:
+                        Console.WriteLine("C'est normal pour un alternant :)");
+                        break;
+                    case < 40000:
+                        if (salary > 30000) {
+                            Console.WriteLine("Tu devrais prendre une formation bac+5 en dev au CESI pour passer ingénieur");
+                        }
+                        break;
                 }
-            } else
+            }
+            else
             {
                 Console.WriteLine("Entrez la taxe en nombre !!!");
             }
-        } else
+        }
+        else
         {
             Console.WriteLine("Entrez le salaire en nombre !!!");
         }
     }
-
-
-    static int CalculateSalaryNet(int salary, int tax)
-    {
-        return (salary * (1 - tax / 100)) / 12;
-    }    
 }
