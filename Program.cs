c90e58bb0622dbb9db34d3decf58f82d7981f754
Rémi Perche <rmi.percher@gmail.com>
2023-03-06 12:24:49 +0100

    Test des TryParse int

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
-﻿using System.Diagnostics;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.WriteLine("Entrez votre salaire brut : ");
-        int salary = int.Parse(Console.ReadLine());
+        if (!int.TryParse(Console.ReadLine(), out int salary))
+        {
+            Console.WriteLine("Ecrivez un nombre !!");
+        }
         Console.WriteLine("Entrez la taxe : ");
-        int tax = int.Parse(Console.ReadLine());
+        if (!int.TryParse(Console.ReadLine(), out int tax))
+        {
+            Console.WriteLine("Ecrivez un nombre !!");
+        }
         Console.WriteLine("Votre salaire est de : " + salary);
         Console.WriteLine("La taxe est de : " + tax);
 
-        Console.WriteLine("Si votre salaire est de " + salary + " et la taxe de " + tax + " alors votre salaire net est de " + AfficherSalaireNet(salary,tax));
+        Console.WriteLine("Si votre salaire est de " + salary + " et la taxe de " + tax + " alors votre salaire net est de " + CalculateSalaryNet(salary, tax));
     }
-    static int AfficherSalaireNet(int salary, int tax)
+
+
+    static int CalculateSalaryNet(int salary, int tax)
     {
         return (salary * (1 - tax / 100)) / 12;
     }    
